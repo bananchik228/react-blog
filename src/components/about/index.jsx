3c528fe077a7e2688233cd7688d913148bf06773
bananchik228 <xzelik@gmail.com>
2020-09-06 20:05:01 +0300

    Написал немного стилей компоненту About

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import './about.scss'
+
 export default () => {
     return (
         <div className="about">
-            <p>Здесь всё обо мне и о моём блоге</p>
+            <h1 className="about__title">Обо мне</h1>
+            <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum voluptate unde quidem quia sapiente. Nostrum tempora qui reprehenderit, nobis libero odio deleniti non in mollitia veniam perferendis neque quos enim vero minima laudantium temporibus deserunt et beatae voluptas ipsa? Error quam debitis soluta modi omnis itaque corrupti fugiat odit!</p>
+
+            <img src="https://picsum.photos/600/300" alt="Какая-то картинка" />
+
+            <p className="about__text about__text_size_small">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+
+            <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, deleniti voluptas non aperiam recusandae corporis voluptatem dicta sunt rerum, mollitia autem quaerat optio cupiditate eaque in eos nostrum maxime debitis.</p>
         </div>
     )
 }
